e5ffb5a91823af364bd34d32169001e0ea0e4e36
manasabharath <86051093+manasabharath@users.noreply.github.com>
2021-07-05 15:57:34 +0530

    Add files via upload

---
+++ ArrayResizeApp/ArrayUtility.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayResizeApp
+{
+    public static class ArrayUtility
+    {
+        public static void Resize(ref int[] array, int newSize)
+        {
+            int[] numbers = array;
+            array = new int[newSize];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                array[i] = numbers[i];
+            }
+        }
+    }
+}
